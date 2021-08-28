9a88e617b6e2e5a80cd240a691cd44ac9d5f27ff
Stuart-Yee <ki6fzb@gmail.com>
2021-08-27 22:09:28 -0700

    MySQL Connection Practice

---
+++ practice/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+# import the class from friend.py
+from friend import Friend
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # call the get all classmethod to get all friends
+    friends = Friend.get_all()
+    for friend in friends:
+        print(friend.first_name, friend.last_name, friend.occupation)
+    return render_template("index.html", friends=friends)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
