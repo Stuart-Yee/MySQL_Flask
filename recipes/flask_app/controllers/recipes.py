195fe13ced48f096f35bf0dc079bca985b1c3351
Stuart-Yee <ki6fzb@gmail.com>
2021-09-09 23:08:39 -0700

    final submission

--- recipes/flask_app/controllers/recipes.py
+++ recipes/flask_app/controllers/recipes.py
@@ -15,39 +15,69 @@ def create_recipe():
     if session["logged_in"] == False | session["logged_in"] == None:
         return redirect("/")
     if request.method == "GET":
-        return render_template("save_recipe.html")
+        edit = False
+        return render_template("save_recipe.html", edit=edit)
     elif request.method == "POST":
         if not Recipe.validate_recipe(request.form):
             return redirect("/recipes/new")
         else:
-            new_id = Recipe.create_recipe(redirect.form)
+            data = {}
+            for key in request.form:
+                data[key] = request.form[key]
+            data["user_id"] = session["user_id"]
+            print(data["user_id"])
+            print(session["user_id"])
+            if data["under_30"] == "True":
+                data["under_30"] = True
+            else:
+                data["under_30"] = False
+            new_id = Recipe.create_recipe(data)
             return redirect(f"/recipes/{new_id}")
     else:
         return "I'm sorry, that request method isn't allowed."
 
 
 @app.route("/recipes/<int:recipe_id>")
-def show_recipe():
+def show_recipe(recipe_id):
     #todo show recipe
-    pass
-    return render_template("show_recipe.html")
+    data = {"id": recipe_id}
+    recipe = Recipe.find_by_id(data)
+    return render_template("show_recipe.html", recipe=recipe)
 
 @app.route("/recipes/edit/<int:recipe_id>", methods=["GET", "POST"])
-def edit_recipe():
-    # todo if session["user_id"] != # todo get recipe user id
-        #return redirect("/logout")
+def edit_recipe(recipe_id):
+    id_data = {"id": recipe_id}
+    recipe = Recipe.find_by_id(id_data)
+    if session["user_id"] != recipe.user_id:
+        return redirect("/logout")
     if request.method == "GET":
-        # todo get recipe from id and pass into edit page
-        return("save_recipe.html")
+        edit = True
+        data = {"id" : recipe_id}
+        recipe = Recipe.find_by_id(data)
+        return render_template("save_recipe.html", edit=edit, recipe=recipe)
     elif request.method == "POST":
-        # todo save recipe in database
-        return redirect(f"/recipes/{recipe_id}")
+        data = {}
+        for key in request.form:
+            data[key] = request.form[key]
+        data["id"] = recipe_id
+        if data["under_30"] == "True":
+            data["under_30"] = True
+        else:
+            data["under_30"] = False
+            if Recipe.validate_recipe(data):
+                Recipe.update_recipe(data)
+                return redirect(f"/recipes/{recipe_id}")
+            else:
+                return redirect(f"/recipes/edit/{recipe_id}")
     else:
         return "I'm sorry, that request method isn't allowed"
 
-@app.route("/recipes/delete/<int:recipe_id>", methods=["POST"])
-def delete_recipe():
-    # todo if session["user_id"] != # todo get recipe user id
-        #return redirect("/logout")
-    # todo delete method for recipe
+@app.route("/recipes/delete/<int:recipe_id>")
+def delete_recipe(recipe_id):
+    id_data = {"id": recipe_id}
+    recipe = Recipe.find_by_id(id_data)
+    if session["user_id"] != recipe.user_id:
+        return redirect("/logout")
+    else:
+        Recipe.delete_recipe(id_data)
     return redirect("/dashboard")
--- recipes/flask_app/models/recipe.py
+++ recipes/flask_app/models/recipe.py
@@ -38,22 +38,39 @@ class Recipe:
 
     @classmethod
     def create_recipe(cls, data):
-        query = "INSERT INTO recipes (name, description, instructions, made_on, under_30, created_on, updated_on, user_id) VALUES (%(name)s, %(description)s, %(instructions)s, %(made_on)s, %(under_30)s, NOW(), NOW(), %(user_id)s);"
+        for row in data:
+            print(row, data[row])
+        query = "INSERT INTO recipes (name, description, instructions, made_on, under_30, created_on, updated_on, recipes.user_id) VALUES (%(name)s, %(description)s, %(instructions)s, %(made_on)s, %(under_30)s, NOW(), NOW(), %(user_id)s);"
         return connectToMySQL("recipes").query_db(query, data)
+
+    @classmethod
+    def update_recipe(cls, data):
+        for key in data:
+            print(key, ":", data[key])
+        query = "UPDATE recipes.recipes SET recipes.name=%(name)s, recipes.instructions=%(instructions)s, recipes.description=%(description)s, recipes.under_30=%(under_30)s, recipes.made_on=%(made_on)s, recipes.updated_on=NOW() WHERE recipes.id = %(id)s;"
+        connectToMySQL("recipes").query_db(query, data)
+
+    @classmethod
+    def delete_recipe(cls, data):
+        query = "DELETE FROM recipes WHERE recipes.id = %(id)s"
+        connectToMySQL("recipes").query_db(query, data)
 
 
     #Validations
     @staticmethod
     def validate_recipe(recipe_info):
         valid_recipe = True
-        if not 3 <= recipe_info["name"] <= 255:
+        if not 3 <= len(recipe_info["name"]) <= 255:
             valid_recipe = False
             flash("Name must be between 3 and 255 characters", "name")
-        if not 20 <= recipe_info["description"] <= 255:
+        if not 20 <= len(recipe_info["description"]) <= 255:
             valid_recipe = False
             flash("Description must be between 20 and 255 characters", "description")
-        if not 20 <= recipe_info["instructions"] <= 255:
+        if not 20 <= len(recipe_info["instructions"]) <= 255:
             valid_recipe = False
             flash("Instructions must be between 20 and 255 characters", "instructions")
+        if recipe_info["made_on"] == None or recipe_info["made_on"] == "" or "made_on" not in recipe_info:
+            valid_recipe = False
+            flash("please pick a date", "date")
         return valid_recipe
 
